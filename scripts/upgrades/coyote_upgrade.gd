298e59b51e53167df25dfaea86e978ff7d74e9c5
Pablo Santana <alu0101480541@ull.edu.es>
2025-01-25 19:25:59 +0000

    feat: coyote upgrade

--- scripts/upgrades/coyote_upgrade.gd
+++ scripts/upgrades/coyote_upgrade.gd
@@ -4,6 +4,10 @@ extends BaseUpgrade
 @export var extra_seconds = 2
 
 func upgrade(player):
+  var run_state = player.get_node("StateMachine/Run")
+  var timer_duration = run_state.coyote_duration
+  print("Previous Coyote Time ", timer_duration )
   print("Added time to Coyote Timer!")
-  var timer: Timer = player.get_node("CoyoteTimer")
-  timer.wait_time += extra_seconds
+  timer_duration += extra_seconds
+  print("New Coyote Time ", timer_duration)
+  run_state.coyote_duration = timer_duration
