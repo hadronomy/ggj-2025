308ea459eff328d432c135c1370fefd93f713fb5
José Ángel Portillo García <114928858+Nyamelot@users.noreply.github.com>
2025-01-26 01:33:49 +0000

    feat: add BubbleSpike Scene (#5)
    * feat: Add Bubble Spikes
    
    * minus changes
    
    * feat: Add Bubble Spikes
    
    * minus changes

---
+++ scripts/bubble_spikes.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@onready var timer = $Timer
+
+func _ready():
+	body_entered.connect(_on_body_entered)
+
+
+func _on_body_entered(body):
+	if body is CharacterBody2D:
+		print("body entered")
+		body.queue_free()
+		timer.start()
+
+func _on_timer_timeout():
+	print("revive")
+	get_tree().reload_current_scene()
